48cf2d1b0dbfc5a0fcfa9845d2a6b78aae1ae2bd
JNBarrachina <jnb.pasadobinario@gmail.com>
2024-05-26 23:28:37 +0200

    Retocando tareas

--- tareas/AdministradorTareas.py
+++ tareas/AdministradorTareas.py
@@ -1,12 +1,78 @@
 import json
 import os
-
 from Tarea import Tarea
 
+"""
+Dado que el objetivo de un administrador de tareas es que recuerde las tareas por tí, 
+se guarda la lista de tareas en un archivo JSON, sobre el cual se pueden agregar, borrar,
+editar y completar tareas.
+
+"""
+
+"""
+El primer paso es crear una clase llamada AdministradorTareas. 
+
+"""
+
 class AdministradorTareas:
+
+    """
+    Sus dos atributos son tanto una lista vacía (donde se listarán las tareas), 
+    como el nombre del archivo donde se van a guardar estas mediante la estructura de datos JSON.
+
+    """
+
     def __init__(self):
         self.tareas= []
         self.filename: str = os.path.dirname(os.path.abspath(__file__)) + '/lista.json'
+
+
+    """
+    Los métodos de la clase AdministradorTareas son los siguientes:
+        1. inicio_administrador(self): devuelve el menu de inicio del administrador de tareas.
+        2. agregar_tarea(self): agrega una nueva tarea a la lista de tareas.
+        3. mostrar_tareas(self): muestra todas las tareas guardadas en el archivo JSON.
+        4. borrar_tarea(self): elimina una tarea de la lista de tareas.
+        5. editar_nombre_tarea(self): edita el nombre de una tarea.
+        6. completar_tarea(self): marca una tarea como completada.
+
+        Se añaden dos métodos adicionales:
+        1. mostrar_tareas_simple(self): muestra solo las tareas guardadas en el archivo JSON.
+        2. modificar_json_simple(self, tareas): actualiza el archivo JSON con las tareas modificadas.
+    
+    """
+
+    def inicio_administrador(self): 
+        print("""\nBIENVENIDO AL ADMINISTRADOR DE TAREAS
+        (Donde usar la memoria es cosa del pasado)
+        ==============================================
+              \n""")
+        print("1. Agregar Tarea")
+        print("2. Mostrar Tareas")
+        print("3. Borrar Tarea")
+        print("4. Editar Tarea")
+        print("5. Completar Tarea")
+        print("6. Salir")
+
+        print("\nElige una opción:")
+        numero = int(input())
+
+        match numero:
+            case 1:
+                self.agregar_tarea()
+            case 2:
+                self.mostrar_tareas()
+            case 3:
+                self.borrar_tarea()
+            case 4:
+                self.editar_nombre_tarea()
+            case 5:
+                self.completar_tarea()
+            case 6:
+                print("\nHasta pronto :)\n")
+                exit()
+            case _:
+                print("\nNo se ha encontrado la función solicitada\n")
 
     def agregar_tarea(self):
         print("\nIngresa el nombre de la nueva tarea:")
@@ -14,7 +80,7 @@ class AdministradorTareas:
         estado = False
 
         try:
-            # Cargar la lista de tareas existente
+        # Cargar la lista de tareas existente
             with open(self.filename, 'r') as file:
                 self.tareas = json.load(file)
         except FileNotFoundError:
@@ -162,36 +228,10 @@ class AdministradorTareas:
         with open(self.filename, 'w') as file:
             json.dump(data, file, indent=4)
 
-    def inicio_administrador(self):
-        print("\nBienvenido al Administrador de Tareas\n")
-        print("1. Agregar Tarea")
-        print("2. Mostrar Tareas")
-        print("3. Borrar Tarea")
-        print("4. Editar Tarea")
-        print("5. Completar Tarea")
-        print("6. Salir")
 
-        print("\nElige una opción:")
-        numero = int(input())
-
-        # TODO: Manejar la excepción
-        match numero:
-            case 1:
-                self.agregar_tarea()
-            case 2:
-                self.mostrar_tareas()
-            case 3:
-                self.borrar_tarea()
-            case 4:
-                self.editar_nombre_tarea()
-            case 5:
-                self.completar_tarea()
-            case 6:
-                print("\nHasta pronto :)\n")
-                exit()
-            case _:
-                print("\nNo se ha encontrado la función solicitada\n")
-        
+"""
+Por último, el if __name__ == "__main__" es el punto de entrada del programa.
+"""
         
 if __name__ == "__main__":
     admin = AdministradorTareas()
