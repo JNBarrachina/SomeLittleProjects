2da7d37ef74b7f1e3af49805ab1017a3b2b36196
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-04-02 13:50:05 +0200

    SimonGame: color sequences fixes

--- simongame/simon.js
+++ simongame/simon.js
@@ -12,7 +12,11 @@ const boardColors = document.getElementsByClassName("boardColor");
 const initGame = document.getElementById("initBtn");
 initGame.addEventListener("click", startGame);
 const readyInfo = document.getElementById("readyText");
+const playerInput = document.getElementById("inputName");
+let playerName;
 const lvlInfo = document.getElementById("lvlText");
+
+const boxScores = document.getElementById("scoreBox");
 
 let colorBtns = ["btngreen", "btnred", "btnyellow", "btnblue"];
 let sequence = [];
@@ -20,8 +24,10 @@ let playerSequence = [];
 let lvl = 1;
 
 function createSequence(){
+    console.log(lvl);
     enableColorBtns();
-    lvlInfo.innerText = `Nivel ${lvl}`;
+    lvlInfo.style.color = "black";
+    lvlInfo.innerText = `Nivel ${lvl} (${playerName})`;
 
     for (i=0; i<lvl; i++){
         let randomColor = Math.floor(Math.random() * colorBtns.length);
@@ -32,24 +38,30 @@ function createSequence(){
     showSequence();
 }
 
-function showSequence(){
-    sequence.forEach(color => {
-        showSingleColor(color);
-    });
+let showColor = 0;
+async function showSequence(){
+    await showSingleColor(showColor)
+    showColor++
+
+    if (showColor == sequence.length){
+        showColor = 0;
+        return;
+    }
+    else{
+        showSequence();
+    }
 }
 
-function showSingleColor(color){
-    document.getElementById(color).classList.add("showColor");
+async function showSingleColor(singleColor){
+    document.getElementById(sequence[singleColor]).classList.add("showColor");
 
-    setTimeout(() => {
-        document.getElementById(color).classList.remove("showColor");
+    await setTimeout(() => {
+        document.getElementById(sequence[singleColor]).classList.remove("showColor");
     }, 1000);
 }
 
-
 function playerTurn(color){
     playerSequence.push(color);
-    console.log(playerSequence);
 
     if (playerSequence.length == sequence.length){
         disableColorBtns();
@@ -62,9 +74,8 @@ function checkSequences(){
         if (playerSequence[indexColor] != sequence[indexColor]){
             lvlInfo.innerText = "HAS PERDIDO";
 
-            setTimeout(() => {
-                location.reload();
-            }, 2000);
+            saveScore();
+            return;
         }
         else{
             if (indexColor == playerSequence.length - 1){
@@ -88,14 +99,17 @@ function nextlvl(){
 }
 
 function startGame(){
+    if (playerInput.value == ""){
+        lvlInfo.style.color = "red";
+        lvlInfo.innerText = "Introduce tu nombre, por favor";
+
+        return;
+    }
+
+    playerName = playerInput.value;
+
+    playerInput.style.display = "none";
     initGame.style.display = "none";
-
-    // let count =  3
-    // setInterval(() => {
-    //     readyInfo.innerText = count
-    //     count--
-    // }, 1000);
-
     createSequence();
 }
 
@@ -112,6 +126,59 @@ function enableColorBtns(){
 
     enableBtns.forEach(btn => {
         btn.classList.remove("disabledColor")
+    });
+}
+
+function saveScore(){
+    playerName = playerName.toLowerCase();
+
+    if (!localStorage.getItem(playerName)){
+        const newPlayerScore = [];
+        newPlayerScore.push(lvl);
+        localStorage.setItem(playerName, JSON.stringify(newPlayerScore));
+    }
+    else{
+        const playerScore = JSON.parse(localStorage.getItem(playerName));
+        playerScore.push(lvl);
+        localStorage.setItem(playerName, JSON.stringify(playerScore));
+    }
+
+    buildChart();
+}
+
+function buildChart(){
+    boxScores.innerHTML = "<canvas id='miGrafico'></canvas>";
+    const ctx = document.getElementById('miGrafico').getContext('2d');
+
+    const playerScoreChart = JSON.parse(localStorage.getItem(playerName));
+    console.log(playerScoreChart);
+
+    const datos = {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+        datasets: [{
+            label: 'Ventas',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: 'green',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        },
+        ]
+    };
+    
+    const miGrafico = new Chart(ctx, {
+        type: 'line', // Tipo de gráfico: 'bar', 'line', 'pie', etc.
+        data: datos,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            animation: {
+                duration: 1000, // Duración en milisegundos
+                easing: 'easeOutBounce' // Tipo de animación
+            }
+        }
     });
 }
 
